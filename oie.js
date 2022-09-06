ba9c99a273de51f13305a031b0b7f1521a5fa603
Anuj Gautam <anujgtm@gmail.com>
2022-09-06 18:28:01 +1200

    sortable common

--- oie.js
+++ oie.js
@@ -28,8 +28,74 @@ window.addEventListener('load',function(){
             localStorage.setItem("options",JSON.stringify(transJson)); 
         }
     }
+
+
+    // Sortable Drag Drop
+    sortable('.sortable-list', {
+        forcePlaceholderSize: true,
+        placeholderClass: 'mb1 bg-navy border border-yellow',
+        hoverClass: 'bg-maroon yellow',
+        itemSerializer: function (item, container) {
+            item.parent = '[parentNode]'
+            item.node = '[Node]'
+            item.html = item.html.replace('<','&lt;')
+            return item
+        },
+        containerSerializer: function (container) {
+            container.node = '[Node]'
+            console.log(container);
+            return container
+        }
+    })
+    let sortableList = document.querySelector('.sortable-list');
+    let buttonS = document.querySelector('.drag-drop-submit'); 
+    let buttonR = document.querySelector('.retry-sort-button');
+    let feedback = document.querySelector('.feedback');
+
+    // Function to randomize List items
+    function randomizeList() {
+        for (var i = sortableList.children.length; i >= 0; i--) {
+            sortableList.appendChild(sortableList.children[Math.random() * i | 0]);
+        }
+    }
+
+    // Sort start EventListener
+    sortable('.sortable-list')[0].addEventListener('sortstart', function(e) {
+        buttonS.disabled = false;
+    });
+
+    // Submit EventListener
+    document.querySelector('.drag-drop-submit').addEventListener('click', function () {
+        // console.log("No option is incorrect!");
+        // let error = document.createElement("p");
+        // error.innerHTML = "No option is incorrect!";
+        this.disabled = true;
+        buttonR.disabled = false;
+        buttonS.classList.toggle('hid');
+        buttonR.classList.toggle('hid');
+        sortable(".sortable-list", "disable");
+        feedback.classList.toggle('hid');
+    });
+
+    // Retry EventListener
+    document.querySelector('.retry-sort-button').addEventListener('click', function () {
+        buttonR.disabled = true;
+        sortable(".sortable-list", "enable");
+        feedback.classList.toggle('hid');
+        buttonS.classList.toggle('hid');
+        buttonR.classList.toggle('hid');
+        randomizeList();
+    });
+
+    // Randomize List
+    randomizeList();
+
+
     
 });
+
+
+
 
 // Set tick for options page
 // <script type="text/javascript">// <![CDATA[
